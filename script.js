57e39e8e67766e67430d19c2c6db14f0fa3afb20
Imrannazirmim <imrannazirmim22@gmail.com>
2025-08-29 22:20:35 +0600

    create history area

--- script.js
+++ script.js
@@ -1,23 +1,79 @@
 const callBtn = document.querySelectorAll("#call-btn");
-const callTitle = document.getElementById("#call-title");
-const callNumber = document.getElementById("#call-number");
+const callTitle = document.getElementById("call-title");
+const callNumber = document.getElementById("call-number");
+const heartCount = document.getElementById("heart-count");
+const coinsCount = document.getElementById("coins-count");
+const copyCount = document.getElementById("copy-count");
+const clearBtn = document.getElementById("clear-btn");
 
 let coins = 100;
-let like = 0;
+let hearts = 0;
+let copy = 0;
 
 for (const callNum of callBtn) {
   callNum.addEventListener("click", function () {
-    if(coins <20){
-        alert('Your coins Not Found.')
-        return;
+    if (coins < 20) {
+      alert("Your coins Not Found.");
+      return;
     }
     const card = callNum.parentElement.parentElement;
-    console.log(card);
-    const cardTitle = card.querySelector('span').innerText;
-    const callNumber = card.querySelector('.call-area span').innerText;   
-    console.log(cardTitle, callNumber);
-    alert('calling' + callTitle + 'provide' + callNumber)
-     coins = coins - 20;
-
+    const cardTitle = card.querySelector("span").innerText;
+    const callNumber = card.querySelector(".call-area span").innerText;
+    alert(`${cardTitle} and Helpline Number ${callNumber}`);
+    coins = coins - 20;
+    coinsCount.innerText = coins;
+    hearts = hearts + 1;
+    heartCount.innerText = hearts;
+    copy = copy + 1;
+    copyCount.innerText = copy;
+    addHistory(cardTitle, callNumber);
   });
 }
+
+// history functionality added
+function addHistory(cardTitle, callNumber) {
+  const historyDiv = document.createElement("div");
+  const historyArea = document.querySelector(".history-area");
+  historyDiv.classList.add("card-history");
+  historyDiv.innerHTML = `
+     <div>
+            <span>${cardTitle}</span>
+            <p>${callNumber}</p>
+        </div>
+        <span>${getCurrentTime()}</span>
+    
+    `;
+  historyArea.appendChild(historyDiv);
+ 
+}
+
+function getCurrentTime() {
+  const d = new Date();
+  let hours = d.getHours().toString();
+  const minutes = d.getMinutes();
+  const second = d.getSeconds();
+  const amToPm = hours >= 12 ? "PM" : "AM";
+  if (hours >= 12) {
+    hours = hours - 12;
+  }
+  if (hours === 12) {
+    hours = 12;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (second < 10) {
+    second = "0" + second;
+  }
+
+  return `${hours}:${minutes}:${second} ${amToPm}`;
+}
+
+ clearBtn.addEventListener("click", function () {
+    const historyItems = document.querySelectorAll('.card-history');
+    for(const history of historyItems){
+        history.remove();
+    }
+    
+  });
